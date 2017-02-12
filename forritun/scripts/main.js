cefb8619d505483f667d0cd3b00803798740e250
Eysteinn <eysteinn.j@gmail.com>
2017-02-12 16:11:01 +0000

    Slider added

--- forritun/scripts/main.js
+++ forritun/scripts/main.js
@@ -15,20 +15,63 @@
 // talk.sayName();
 
 
-var r = new XMLHttpRequest();
+// var r = new XMLHttpRequest();
 
-r.open("GET", "path/to/api", true);
-r.onreadystatechange = function() {
-	if (r.readyState != 4 || r.status != 200) return; 
-	const response = JSON.parse(r.responseText)
-	const container = document.createElement("div");
-	const image = document.createElement("img");
-	const title = document.createElement("h1");
-	image.src = JSON.parse(r.response);
-	title.inneHTML = response.original_title;
-	container.append(image);
-	container.append(title);
-	document.body.append(container);
-}
+// r.open("GET", "path/to/api", true);
+// r.onreadystatechange = function() {
+// 	if (r.readyState != 4 || r.status != 200) return; 
+// 	const response = JSON.parse(r.responseText)
+// 	const container = document.createElement("div");
+// 	const image = document.createElement("img");
+// 	const title = document.createElement("h1");
+// 	image.src = JSON.parse(r.response);
+// 	title.inneHTML = response.original_title;
+// 	container.append(image);
+// 	container.append(title);
+// 	document.body.append(container);
+// }
 
-r.send("banana=yellow");
+// r.send("banana=yellow");
+$(document).ready(function(){
+$('.responsive').slick({
+  prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+  nextArrow: '<button type="button" class="slick-next">Next</button>',
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3.5,
+  centerMode: false,
+  centerPadding: '40px',
+  responsive:[	
+	{
+      breakpoint: 1920,
+      settings: {
+        slidesToShow: 3.5,
+        slidesToScroll: 2,
+        infinite: false,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+      	centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: '20px'
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+});
+
+
