66349513af46d401a4b3dc4d6caf334c34cc7e19
Eysteinn <eysteinn.j@gmail.com>
2017-02-13 16:45:41 +0000

    About movie page added

--- forritun/scripts/main.js
+++ forritun/scripts/main.js
@@ -51,10 +51,18 @@ $('.responsive').slick({
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 1024,
       settings: {
-        slidesToShow: 1,
-        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 3.5,
+        slidesToScroll: 2,
+        infinite: false,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2.5,
         centerPadding: '20px',
         slidesToScroll: 1
       }
@@ -62,10 +70,97 @@ $('.responsive').slick({
     {
       breakpoint: 480,
       settings: {
+      	infinite:true,
       	centerMode: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         centerPadding: '20px'
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.responsiveCast').slick({
+  // prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+  // nextArrow: '<button type="button" class="slick-next">Next</button>',
+  infinite: false,
+  speed: 300,
+  slidesToShow: 5,
+  centerMode: false,
+  responsive:[	
+	{
+      breakpoint: 1920,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        infinite: false,
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        infinite: false,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.responsiveStills').slick({
+  prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+  nextArrow: '<button type="button" class="slick-next">Next</button>',
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  centerMode: false,
+  responsive:[	
+	{
+      breakpoint: 1920,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        infinite: false,
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        infinite: false,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1.5,
+        slidesToScroll: 1,
       }
     }
     // You can unslick at a given breakpoint now by adding:
