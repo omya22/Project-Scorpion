c243511f953c4627544831089565f1959bc2a59c
Eysteinn <eysteinn.j@gmail.com>
2017-02-13 17:43:46 +0000

    update port

--- forritun/app.js
+++ forritun/app.js
@@ -5,7 +5,7 @@ const ejs = require('ejs')
 const fetch = require('node-fetch');
 const app = express();
 const httpServer = http.createServer(app);
-const port = 3000;
+const port = 4000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
