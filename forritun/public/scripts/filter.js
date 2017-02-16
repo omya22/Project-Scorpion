9fb000d3aeadd57d97643a77a97fd803d4613c60
Eysteinn <eysteinn.j@gmail.com>
2017-02-16 14:24:23 +0000

    update

--- forritun/public/scripts/filter.js
+++ forritun/public/scripts/filter.js
@@ -58,7 +58,7 @@ var $quicksearch = $('#quicksearch').keyup( debounce( function() {
 $('.button-group').each( function( i, buttonGroup ) {
   var $buttonGroup = $( buttonGroup );
   $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked');
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
     $( this ).addClass('is-checked');
   });
 });
